e0dad73951df446c85a2a6025b64a4fcc815a32e
prasannavadhananpsg <44691862+prasannavadhananpsg@users.noreply.github.com>
2018-12-28 10:20:01 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -7,23 +7,7 @@ router.post('/saveAsset', function (req, res) {
    var data = req.body;    
    db.collection("mycoll").insert(data)
 });
-/*router.post('/updateUser', function (req, res) {
-    var db = mongodb.getDb();
-    var data = req.body;    
-    db.collection("mycoll").save({_id:ObjectId(),data})      
-});*/
-/*router.post('/deleteAsset', function (req, res) {
-    var db = mongodb.getDb();
-    var data = req.body;    
-    db.collection("mycoll").delete(data)   
-});*/
-router.get('/getAllAssets', function (req, res) {
-    var db = mongodb.getDb();
-    var coll = db.collection("mycoll");
-    coll.find({}).toArray(function (err, data) {
-        res.send(data);
-    });
-}); 
+
 
 // On localhost:3000/welcome
 router.get('/welcome', function (req, res) {
